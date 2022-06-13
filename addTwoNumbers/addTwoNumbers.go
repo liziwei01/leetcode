45c2c8930b05f9930c4b6d6cae047163d234f5a3
liziwei01 <alssylk@gmail.com>
2022-06-13 15:59:18 +0800

    init

---
+++ addTwoNumbers/addTwoNumbers.go
@@ -0,0 +1,35 @@
+/*
+ * @Author: liziwei01
+ * @Date: 2022-06-13 09:25:31
+ * @LastEditors: liziwei01
+ * @LastEditTime: 2022-06-13 09:57:26
+ * @Description: file content
+ */
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	var ret *ListNode = &ListNode{}
+	ret.Val = l1.Val + l2.Val
+
+	if ret.Val < 10 {
+		ret.Next = addTwoNumbers(l1.Next, l2.Next)
+	} else {
+		ret.Val = ret.Val - 10
+		ret.Next = addTwoNumbers(l1.Next, l2.Next)
+		ret.Next = addTwoNumbers(ret.Next, &ListNode{Val: 1})
+	}
+
+	return ret
+}
---
+++ twoSum/twoSum.go
@@ -0,0 +1,34 @@
+/*
+ * @Author: liziwei01
+ * @Date: 2022-06-13 09:15:39
+ * @LastEditors: liziwei01
+ * @LastEditTime: 2022-06-13 09:57:13
+ * @Description: file content
+ */
+package main
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	ret := []int{}
+
+	if len(nums) < 2 {
+		return []int{}
+	}
+
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				m[i] = 1
+				m[j] = 1
+			}
+		}
+	}
+
+	for k := range m {
+		if m[k] == 1 {
+			ret = append(ret, k)
+		}
+	}
+
+	return ret
+}
